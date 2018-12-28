2ef06a87e142ac9a65708d92464d4531f21009f0
Paula McKinley <pmknyc@optimum.net>
2018-12-28 20:35:51 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,12 +1,14 @@
-require_relative './dance_module.rb'
-require_relative './class_methods_module.rb'
+# require_relative './dance_module.rb'
+# require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
-class Kid 
-  include Dance
- 
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
   end
-end #class end 
+end #class end
